41fcec1afabc830a30bcae10bd9dba8b7a049235
guweimo <lyfw244885@hotmail.com>
2018-11-14 21:50:12 +0800

    update

---
+++ WebGame/mxtanGame/ball.js
@@ -0,0 +1,38 @@
+
+var Ball = function() {
+    var image = imageFromPath('ball.png')
+    var o = {
+        image: image,
+        x: 100,
+        y: 200,
+        speedX: 10,
+        speedY: 10,
+        fired: false,
+    }
+    o.fire = function() {
+        o.fired = true
+    }
+    o.stop = function() {
+        o.fired = false
+    }
+    o.move = function() {
+        if (o.fired) {
+            // log('move')
+            if (o.x < 0 || o.x > 400) {
+                o.speedX = -o.speedX
+            }
+            if (o.y < 0 || o.y > 300) {
+                o.speedY = -o.speedY
+            }
+            // move
+            o.x += o.speedX
+            o.y += o.speedY
+        }
+    }
+    
+    o.bound = function() {
+        o.speedY *= -1
+    }
+
+    return o
+}
---
+++ WebGame/mxtanGame/utils.js
@@ -0,0 +1,18 @@
+var log = console.log.bind(console)
+
+var imageFromPath = function(path) {
+    var img = new Image()
+    img.src = path
+    return img
+}
+
+var rectIntersects = function(a, b)  {
+    var o = a;
+    if (b.y + o.y && b.y < o.y + b.image.height) {
+        if (b.x > o.x && b.x < o.x + o.image.width) {
+            log('相撞')
+            return true
+        }
+    }
+    return false;
+}
