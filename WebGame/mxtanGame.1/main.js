9e7651666296b734818e6c4b635ab972cb7e0801
guweimo <lyfw244885@hotmail.com>
2018-11-19 13:57:30 +0800

    update

---
+++ WebGame/mxtanGame.1/main.js
@@ -0,0 +1,56 @@
+
+var loadLevel = function(game, n) {
+    n = n - 1
+    var level = levels[n]
+    var blocks = []
+    for (let i = 0; i < level.length; i++) {
+        var p = level[i]
+        let b = Block(game, p)
+        b.x = p[0]
+        b.y = p[1]
+        blocks.push(b)
+    }
+    return blocks
+}
+
+var blocks = []
+var enableDebugMode = function(game, enable) {
+    if (!enable) {
+        return
+    }
+    window.paused = false
+    // 这是为了 debug
+    window.addEventListener('keydown', function(event) {
+        var k = event.key
+        if (k == 'p') {
+            // 暂停功能
+            window.paused = !window.paused
+        } else if ('1234567'.includes(k)) {
+            // 为了 debug 临时加的载入关卡功能
+            blocks = loadLevel(game, Number(k))
+        } 
+    })
+    // 控制速度
+    document.querySelector('#id-input-speed').addEventListener('input', function(event) {
+        var input = event.target
+        window.fps = Number(input.value)
+    })
+}
+
+var __main = function() {
+    
+    var images = {
+        ball: 'img/ball.png',
+        block: 'img/block.png',
+        paddle: 'img/paddle.png'
+    }
+    
+    var game = MxtanGame.instance(30, images, function(game) {
+        var s = SceneTitle.new(game)
+        game.runWithScene(s)
+    })
+
+    enableDebugMode(game, true)
+}
+
+__main()
