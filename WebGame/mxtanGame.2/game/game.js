46b08650f2f2f30c7b2bf896fe43cb3c38e7bbc5
guweimo <lyfw244885@hotmail.com>
2018-11-20 20:09:12 +0800

    update

---
+++ WebGame/mxtanGame.2/game/game.js
@@ -0,0 +1,111 @@
+// mxtan
+class MxtanGame {
+    constructor(fps, images, runCallBack) {
+        window.fps = fps
+        this.images = images
+        this.runCallBack = runCallBack
+        // 
+        this.scene = null
+        this.actions = {}
+        this.keydowns = {}
+        this.canvas = document.querySelector('#id-canvas')
+        this.context = this.canvas.getContext('2d')
+        //events
+        var self = this
+        window.addEventListener('keydown', event => {
+            this.keydowns[event.key] = true;
+        })
+        window.addEventListener('keyup', function(event) {
+            self.keydowns[event.key] = false;
+        })
+        this.init()
+    }
+
+    static instance(...args) {
+        this.i = this.i || new this(...args)
+        return this.i
+    }
+    drawImage(img) {
+        this.context.drawImage(img.texture, img.x, img.y)
+    }
+    // update
+    update() {
+        this.scene.update()
+    }
+    // draw
+    draw() {
+        this.scene.draw()
+    }
+
+    registerAction(key, callback) {
+        this.actions[key] = callback
+    }
+    runloop() {
+        // events
+        var g = this
+        var actions = Object.keys(g.actions)
+        for (var i = 0; i < actions.length; i++) {
+            var key = actions[i]
+            if (g.keydowns[key]) {
+                // 如果按键被按下，调用注册的action
+                g.actions[key]()
+            }
+        }
+        // update
+        g.update()
+        // clear
+        g.context.clearRect(0, 0, g.canvas.width, g.canvas.height)
+        // draw
+        g.draw()
+        // next run loop
+        setTimeout(function() {
+            g.runloop()
+        }, 1000/window.fps)
+    }
+    textureByName(name) {
+        let g = this
+        let img = g.images[name]
+        // let image = {
+        //     w: img.width,
+        //     h: img.height,
+        //     image: img,
+        // }
+        return img
+    }
+    runWithScene(scene) {
+        let g = this
+        g.scene = scene
+        // 开始运行程序
+        setTimeout(function() {
+            g.runloop()
+        }, 1000/window.fps)
+    }
+    replaceScene(scene) {
+        this.scene = scene
+    }
+    run() {
+        this.runCallBack(this)
+    }
+    init() {
+        let g = this
+        let loads = []
+        // 预先载入所有图片
+        var names = Object.keys(g.images)
+        for (let i = 0; i < names.length; i++) {
+            let name = names[i]
+            let path = this.images[name]
+            let img = new Image()
+            img.src = path
+            img.onload = function() {
+                // 存入 g.images 中
+                g.images[name] = img
+                // 所有图片都成功载入之后，调用 run
+                loads.push(1)
+                if (loads.length == names.length) {
+                    g.run()
+                }
+            }
+            
+        }
+    }
+}
---
+++ WebGame/mxtanGame.2/game/mxtan_scene.js
@@ -0,0 +1,32 @@
+class MxtanScene {
+    constructor(game) {
+        this.game = game
+        this.debugModeEnabled = true
+        this.elements = []
+    }
+    static new(game) {
+        let i = new this(game)
+        return i
+    }
+    addElement(img) {
+        img.scene = this
+        this.elements.push(img)
+    }
+    draw() {
+        for (let e of this.elements) {
+            e.draw()
+        }
+    }
+    update() {
+        if (this.debugModeEnabled) {
+            for (let i = 0; i < this.elements.length; i++) {
+                let e = this.elements[i]
+                e.debug && e.debug()
+            }
+        }
+        for (let i = 0; i < this.elements.length; i++) {
+            let e = this.elements[i]
+            e.update()
+        }
+    }
+}
